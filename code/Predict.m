79c0384118368ba9846997f201a02dcbeb4ef9a1
JunHuangUCAS <huangjun_cs@163.com>
2015-08-31 22:14:07 +0800

    code

--- code/Predict.m
+++
@@ -1,7 +0,0 @@
-function predict_target = Predict(Outputs,tau)
-    predict_target = zeros(size(Outputs));
-    num_class = size(Outputs,1);
-    for l = 1:num_class
-        predict_target(l,:) = Outputs(l,:) > tau(1,l);
-    end
-end
